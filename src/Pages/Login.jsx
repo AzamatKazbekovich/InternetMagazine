f54d01e2c38d5711961363e58f681d0b72df04d7
Azamat Sarsengaliev <abudiga@bk.ru>
2023-01-16 15:32:44 +0400

    valid

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,97 @@
+import React, { useEffect, useState } from "react";
+import MyButton from "../Components/UI/Button/MyButton";
+import MyInput from "../Components/UI/Input/MyInput";
+import classes from "./Login.module.css";
+
+const Login = () => {
+  const login = (event) => {
+    event.preventDefault();
+
+    localStorage.setItem();
+  };
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailDirty, setEmailDirty] = useState(false);
+  const [passwordDirty, setPasswordDirty] = useState(false);
+  const [emailError, setEmailError] = useState("Email не может быть пустым");
+  const [passwordError, setPasswordError] = useState(
+    "Пароль не может быть пустым"
+  );
+  const [formValid, setFormValid] = useState(false);
+
+  useEffect(() => {
+    if (emailError || passwordError) {
+      setFormValid(false);
+    } else {
+      setFormValid(true);
+    }
+  }, [emailError, passwordError]);
+
+  const emailHandler = (e) => {
+    setEmail(e.target.value);
+    const re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(String(e.target.value).toLowerCase())) {
+      setEmailError("Некорректный email");
+    } else {
+      setEmailError("");
+    }
+  };
+
+  const passwordHandler = (e) => {
+    setPassword(e.target.value);
+    if (e.target.value.length < 3 || e.target.value.length > 8) {
+      setPasswordError("Пароль дожен быть длиннее 3 и больше 8 символов");
+      if (!e.target.value) {
+        setPasswordError("Пароль не может быть пустым");
+      }
+    } else {
+      setPasswordError("");
+    }
+  };
+
+  const blurHandler = (e) => {
+    switch (e.target.name) {
+      case "email":
+        setEmailDirty(true);
+        break;
+      case "password":
+        setPasswordDirty(true);
+        break;
+    }
+  };
+
+  return (
+    <div className={classes.login}>
+      <div className={classes.title}>Интернет-магазин</div>
+      <form onSubmit={login} className={classes.IaB}>
+        {emailDirty && emailError && (
+          <div style={{ color: "red" }}>{emailError}</div>
+        )}
+        <MyInput
+          onChange={(e) => emailHandler(e)}
+          value={email}
+          onBlur={(e) => blurHandler(e)}
+          name="email"
+          type="text"
+          placeholder="Введите свой Email"
+        />
+        {passwordDirty && passwordError && (
+          <div style={{ color: "red" }}>{passwordError}</div>
+        )}
+        <MyInput
+          onChange={(e) => passwordHandler(e)}
+          value={password}
+          onBlur={(e) => blurHandler(e)}
+          name="password"
+          type="password"
+          placeholder="Введите свой пароль"
+        />
+        <MyButton disabled={!formValid}>Войти</MyButton>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
